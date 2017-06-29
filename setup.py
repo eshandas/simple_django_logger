6def9f79a14c99c58e3feb9d0a5f719e18f6bc55
Eshan Das <eshandasnit@gmail.com>
2017-06-29 17:36:49 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 # Read more here: https://www.codementor.io/arpitbhayani/host-your-python-package-using-github-on-pypi-du107t7ku
 
 setup(
     name='simple_django_logger',
-    packages=[
-        'simple_django_logger',  # this must be the same as the name above
-        'simple_django_logger.middleware',
-        'simple_django_logger.migrations'],
+    # packages=[
+    #     'simple_django_logger',  # this must be the same as the name above
+    #     'simple_django_logger.middleware',
+    #     'simple_django_logger.migrations'],
+    packages=find_packages(),
     include_package_data=True,
-    version='0.1.0',
+    version='0.1.2',
     description='A basic logger for Django',
     author='Eshan Das',
     author_email='eshandasnit@gmail.com',
     url='https://github.com/eshandas/simple_django_logger',  # use the URL to the github repo
-    download_url='https://github.com/eshandas/simple_django_logger/archive/0.1.0.tar.gz',  # Create a tag in github
+    download_url='https://github.com/eshandas/simple_django_logger/archive/0.1.2.tar.gz',  # Create a tag in github
     keywords=['django', 'logger'],
     classifiers=[],
 )
